94370846efc394c702d37fd95c7a41ed6134b98f
326268 <91246881+326268@users.noreply.github.com>
2021-09-30 16:31:14 +0800

    20210930

--- main.ts
+++ main.ts
@@ -1,3 +1,39 @@
+basic.showLeds(`
+    # # # # #
+    # . . . #
+    # . . . #
+    # . . . #
+    # # # # #
+    `)
+basic.showLeds(`
+    . . . . .
+    . # # # .
+    . # . # .
+    . # # # .
+    . . . . .
+    `)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . # . .
+    . . . . .
+    . . . . .
+    `)
+basic.showLeds(`
+    . . . . .
+    . # # # .
+    . # . # .
+    . # # # .
+    . . . . .
+    `)
+basic.showLeds(`
+    # # # # #
+    # . . . #
+    # . . . #
+    # . . . #
+    # # # # #
+    `)
+basic.clearScreen()
 for (let y = 0; y <= 4; y++) {
     for (let x = 0; x <= y; x++) {
         led.plot(x, y)
@@ -5,3 +41,23 @@ for (let y = 0; y <= 4; y++) {
     }
 }
 basic.clearScreen()
+for (let x = 0; x <= 4; x++) {
+    for (let y = 0; y <= x; y++) {
+        led.plot(x, y)
+        basic.pause(100)
+    }
+}
+basic.clearScreen()
+for (let y = 0; y <= 4; y++) {
+    for (let x = 0; x <= 4; x++) {
+        led.plot(x, y)
+        basic.pause(100)
+    }
+}
+basic.clearScreen()
+for (let x = 0; x <= 4; x++) {
+    for (let y = 0; y <= 4; y++) {
+        led.plot(x, y)
+        basic.pause(100)
+    }
+}
