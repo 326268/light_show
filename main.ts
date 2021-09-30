2eaed1b43ae6ba375b0f9a9a4acb7a5040431f14
326268 <91246881+326268@users.noreply.github.com>
2021-09-30 16:09:19 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,7 @@
-basic.forever(function () {
-    basic.showLeds(`
-        . # . # .
-        . # . # .
-        # . . . #
-        . . . . .
-        . # # # .
-        `)
-})
+for (let y = 0; y <= 4; y++) {
+    for (let x = 0; x <= y; x++) {
+        led.plot(x, y)
+        basic.pause(100)
+    }
+}
+basic.clearScreen()
